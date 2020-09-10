eb8827c9a4a22c2157701742b4c92e1bec6fe09c
aashimasaxena91 <70135562+aashimasaxena91@users.noreply.github.com>
2020-09-10 13:17:34 +0530

    admin and user login page

---
+++ admin_login.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Admin Login Page</title>
+    <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="HandheldFriendly" content="true">
+    <link rel="stylesheet" href="admin_login.css">
+    <link href="https://fonts.googleapis.com/css2?family=Merriweather&family=Patua+One&family=Source+Sans+Pro:wght@300&display=swap" rel="stylesheet">
+  </head>
+  <body>
+    <div class="loginbox">
+      <div class="avatarbox">
+        <img src="images/boss.png" alt="">
+      </div>
+          <h1>Admin Login</h1>
+          <div class="formbox">
+
+            <form class="" action="admin_login.php" method="post">
+              <p>Username</p>
+              <input type="text" name="" placeholder="Enter Username">
+              <p>Password</p>
+              <input type="password" name="" placeholder="Enter Password"> <br><br>
+              <button type="submit" class="btn" name="">Login</button>
+            </form>
+
+          </div>
+    </div>
+
+  </body>
+</html>
---
+++ user_login.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>User Login Page</title>
+    <link rel="stylesheet" href="user_login.css">
+    <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="HandheldFriendly" content="true">
+    <link href="https://fonts.googleapis.com/css2?family=Merriweather&family=Patua+One&family=Source+Sans+Pro:wght@300&display=swap" rel="stylesheet">
+  </head>
+  <body>
+    <div class="loginbox">
+      <div class="avatarbox">
+        <img src="images/interview.png" alt="">
+      </div>
+          <h1>Student Login</h1>
+          <div class="formbox">
+
+            <form class="" action="user_login.php" method="post">     <!-- The action is initially to this page itself, later we will change it-->
+              <p>Username</p>
+              <input type="text" name="" placeholder="Enter Username">
+              <p>Password</p>
+              <input type="password" name="" placeholder="Enter Password"> <br><br>
+              <button type="submit" class="btn" name="">Login</button>
+            </form>
+          </div>
+          <br> <p>Haven't registered yet? <a href="user_registration.php">Sign up here</a></p>
+    </div>
+
+  </body>
+</html>

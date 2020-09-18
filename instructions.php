7b169bbb39991f5f08f29f53d74f5e8d8b1c3dfa
garvit-bhardwaj <12as1911045@gmail.com>
2020-09-18 13:14:09 +0530

    added UI for quiz and added timer

--- instructions.php
+++ instructions.php
@@ -4,6 +4,10 @@ if(!isset($_SESSION['name']))
 {
 	header("location:user_login.php");
 }
+$quizid=$_GET['n'];
+$total=$_GET['total'];
+$dur=$_GET['dur'];
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -14,6 +18,21 @@ if(!isset($_SESSION['name']))
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
     <link rel="stylesheet" href="userportal.css">
     <title>Student Portal</title>
+    <style>
+    .goback{
+      width:150px;
+      border-radius:7px;
+      color:white;
+      background:green;
+      text-decoration:none;
+      line-height:45px;
+      margin:auto;
+      text-align:center;
+    }
+    .goback:hover{
+      background:#8aa779;
+    }
+    </style>
 </head>
 <body>
     <header>
@@ -39,10 +58,11 @@ if(!isset($_SESSION['name']))
     echo '</div>';
 
     ?>
-    <a href="#" class="goback">Go to Quiz</a>
     
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+
+    <a href="startquiz.php?n=<?php echo $quizid ?>&total=<?php echo $total ?>&dur=<?php echo $dur ?>" class="goback">Go to Quiz</a>
 </body>
 </html>
